a9db062200545b04a9f0a2f6edd995782612bc9a
Assym <asimo_555@mail.ru>
2022-06-30 04:26:56 +0600

    коммит 2 добавил модель Task и сделал migration, также добавил admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'created_at']
+    list_display_links = ['description']
+    list_filter = ['status']
+    search_fields = ['description']
+    fields = ['description', 'status', 'created_at']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Task, TaskAdmin)
