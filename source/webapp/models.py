63da145396fe2726f2585d53f96c30e5c16d77cf
Assym <asimo_555@mail.ru>
2022-07-03 23:06:31 +0600

    исправил models.py discription TextField

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -5,7 +5,7 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 
 class Task(models.Model):
-    description = models.CharField(max_length=3000, null=False, verbose_name="Описание")
+    description = models.TextField(max_length=3000, null=False, verbose_name="Описание")
     status = models.CharField(max_length=50, null=False, verbose_name="Статус", default=STATUS_CHOICES[0][0],
                               choices=STATUS_CHOICES)
     created_at = models.DateField(null=True, blank=True, verbose_name="Дата создания")
