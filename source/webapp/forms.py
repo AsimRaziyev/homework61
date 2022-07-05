c50d26b0843d918b37d72b2b2dfc5e4cac8ceb43
Assym <asimo_555@mail.ru>
2022-07-06 00:33:19 +0600

    исправил dump.json,в формс добавил ChoiceField и убрал max_length=60

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -3,7 +3,8 @@ from django.forms import widgets
 
 
 
+
 class TaskForm(forms.Form):
     description = forms.CharField(max_length=3000, required=True, label='Description', widget=widgets.Textarea(attrs={"cols": 30, "rows": 3}))
-    status = forms.CharField(max_length=60, required=True, label='Status')
+    status = forms.ChoiceField(required=True, choices=[('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')], label='Status')
     created_at = forms.CharField(max_length=50, required=True, label='Created_at')
