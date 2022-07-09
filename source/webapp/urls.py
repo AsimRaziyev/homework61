67a84ec031621ecb239bbe911763c45989b819db
Assym <asimo_555@mail.ru>
2022-07-09 22:53:33 +0600

    изменил функции index_view в класс IndexView также task_view в класс TaskView

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import index_view, create_task, task_view, delete_task, update_task
+from webapp.views import IndexView, create_task, TaskView, delete_task, update_task
 
 urlpatterns = [
-    path("", index_view, name="index"),
+    path("", IndexView.as_view(), name="index"),
     path("tasks/add/", create_task, name="create_task"),
-    path("task/<int:pk>/", task_view, name="task_view"),
+    path("task/<int:pk>/", TaskView.as_view(), name="task_view"),
     path('task/<int:pk>/update', update_task, name="update_task"),
     path('task/<int:pk>/delete', delete_task, name="delete_task"),
 
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView
 from webapp.models import Task
 from webapp.models import STATUS_CHOICES
 from webapp.forms import TaskForm
 
 
-def index_view(request):
-    tasks = Task.objects.order_by("-created_at")
-    context = {"tasks": tasks}
-    return render(request, "index.html", context)
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        tasks = Task.objects.order_by("-created_at")
+        context = {"tasks": tasks}
+        return render(request, "index.html", context)
 
 
-def task_view(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    return render(request, 'task_view.html', {"task": task})
+class TaskView(TemplateView):
+    template_name = "task_view.html"
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get("pk")
+        task = get_object_or_404(Task, pk=pk)
+        kwargs["task"] = task
+        return super().get_context_data(**kwargs)
 
 
 def create_task(request):
