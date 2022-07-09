a71dc57fc8edf047e754cb58ab74e82adabfc4d0
Assym <asimo_555@mail.ru>
2022-07-09 16:26:25 +0600

    forms.py

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from django.forms import widgets
 
 
-
-
 class TaskForm(forms.Form):
-    description = forms.CharField(max_length=3000, required=True, label='Description', widget=widgets.Textarea(attrs={"cols": 30, "rows": 3}))
-    status = forms.ChoiceField(required=True, choices=[('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')], label='Status')
+    description = forms.CharField(max_length=3000, required=True, label='Description',
+                                  widget=widgets.Textarea(attrs={"cols": 30, "rows": 3}))
+    status = forms.ChoiceField(required=True,
+                               choices=[('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')],
+                               label='Status')
     created_at = forms.CharField(max_length=50, required=True, label='Created_at')
