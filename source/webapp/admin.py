1dd39a2ae3c53ed7ce13e528b5935b1e8a03e5bb
Assym <asimo_555@mail.ru>
2022-07-09 18:16:48 +0600

    добавил Коммит в модель, также дата изменения

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -8,8 +8,9 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'description', 'status', 'task_name', 'created_at']
     list_display_links = ['description']
     list_filter = ['status']
-    search_fields = ['description']
-    fields = ['description', 'status', 'task_name', 'created_at']
+    search_fields = ['description', 'task_name']
+    fields = ['description', 'status', 'task_name', 'updated_at']
+    readonly_fields = ['updated_at']
 
 
 admin.site.register(Task, TaskAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,13 +4,19 @@ from django.db import models
 STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
 
 
-class Task(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата изменения")
+
+    class Meta:
+        abstract = True
+
+
+class Task(BaseModel):
     task_name = models.CharField(max_length=50, null=False, verbose_name="Названия задачи")
     description = models.TextField(max_length=3000, null=False, verbose_name="Описание")
     status = models.CharField(max_length=60, null=False, verbose_name="Статус", default=STATUS_CHOICES,
                               choices=STATUS_CHOICES)
-    created_at = models.DateField(null=True, blank=True, verbose_name="Дата создания")
-
 
     def __str__(self):
         return f"{self.id}. {self.description}: {self.status}"
@@ -19,3 +25,17 @@ class Task(models.Model):
         db_table = "tasks"
         verbose_name = "Задача"
         verbose_name_plural = "Задачи"
+
+
+class Comment(BaseModel):
+    text = models.TextField(max_length=400, verbose_name="Комментарий")
+    author = models.CharField(max_length=40, null=True, default="Аноним", verbose_name="Автор")
+    task = models.ForeignKey("webapp.Task", on_delete=models.CASCADE, related_name="comments", verbose_name="Задача")
+
+    def __str__(self):
+        return f"{self.id}. {self.text}: {self.author}"
+
+    class Meta:
+        db_table = "comments"
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
